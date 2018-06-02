eb383469a38d6422ed95f42439b844476dc4094c
jojo020595 <jonathan.espiard@epita.fr>
2018-06-02 17:16:35 +0200

    [UPDATE] Create a table for the videos

--- Database/CreateYoutubeRepeat.sql
+++ Database/CreateYoutubeRepeat.sql
@@ -13,9 +13,15 @@ CREATE TABLE T_User (
   Lastname          VARCHAR(50)     NOT NULL
 );
 
+CREATE TABLE T_Videos (
+  URL				VARCHAR(256)             PRIMARY KEY NOT NULL ,
+  Name      VARCHAR(128)             NOT NULL
+);
+
+
 CREATE TABLE T_History (
-   Id                SERIAL         PRIMARY KEY NOT NULL,
-  URL				VARCHAR(256)            NOT NULL ,
+  Id               SERIAL           PRIMARY KEY NOT NULL,
+  URL				        VARCHAR(256)    NOT NULL references T_Videos (URL) ,
   Id_user           BIGINT          NOT NULL references T_User (Id),
   DatePost          TIMESTAMP       NOT NULL
 );
@@ -23,14 +29,13 @@ CREATE TABLE T_History (
 CREATE TABLE T_Favorite (
   Id                SERIAL          PRIMARY KEY NOT NULL,
   Id_user           BIGINT          NOT NULL references T_User (Id),
-  Id_videos        BIGINT     NOT NULL references T_History (Id),
-  DatePost          TIMESTAMP       NOT NULL
+  Id_videos         VARCHAR(256)    NOT NULL references T_Videos (URL)
 );
 
 CREATE TABLE T_Comment (
   Id                SERIAL          PRIMARY KEY NOT NULL,
   Id_user           BIGINT          NOT NULL references T_User (Id),
-  Id_videos        BIGINT     NOT NULL references T_History (Id),
+  Id_videos         VARCHAR(256)    NOT NULL references T_Videos (URL) ,
   DatePost          TIMESTAMP       NOT NULL,
   Grade             INT             CHECK (Grade > 0 AND Grade <= 5),
   Comment           VARCHAR (1024)
